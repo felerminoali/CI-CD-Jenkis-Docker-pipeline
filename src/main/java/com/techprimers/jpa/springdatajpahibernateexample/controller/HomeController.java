1d364b699c99bb0d9d6561e7d1a4a549d3d7d5e7
felerminoali <felerminoali@gmail.com>
2020-03-04 16:38:12 +0200

    First commit

---
+++ src/main/java/com/techprimers/jpa/springdatajpahibernateexample/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.techprimers.jpa.springdatajpahibernateexample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/index")
+    public String index(){
+        return "Hellp World";
+    }
+
+}
